d734071e9253973f146dd2b561398000b8c31324
Ryu Seino <seino@g-gent.com>
2017-09-04 10:31:49 +0900

    change migration, and add seed.

---
+++ database/migrations/2017_09_04_093028_create_master_service.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMasterService extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/seeds/ServicesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Service;
+
+class ServicesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Service::create([
+            'id' => 0,
+            'name' => 'twitter',
+        ]);
+    }
+}
