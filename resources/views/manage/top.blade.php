7097f0cf3a218ac344b83507d54cfd4d6c4575fb
Ryu Seino <huntinghighandlow.rs@gmail.com>
2018-04-20 15:00:57 +0900

    code format

--- resources/views/manage/top.blade.php
+++ resources/views/manage/top.blade.php
@@ -10,10 +10,10 @@
 
     {!! Form::open(['url' => 'diaries']) !!}
     {{ method_field('patch') }}
-    <div class="form-group ">
+    <div class="form-group">
         {!! Form::label('diary-body', '本日の日記') !!}
-        {!! Form::textarea('diary-body', null, ['class' => $errors->has('diary-body') ? 'form-control is-invalid' : 'form-control'] ) !!}
-        <div class="invalid-feedback">{{$errors->first('diary-body')}}</div>
+        {!! Form::textarea('diary-body', null, ['class' =>$errors->has('diary-body') ? 'form-control is-invalid' : 'form-control'] ) !!}
+        <span class="invalid-feedback">{{$errors->first('diary-body')}}</span>
     </div>
     <div class="form-group">
         {!! Form::submit('更新', ['class' => 'btn btn-primary form-control']) !!}
