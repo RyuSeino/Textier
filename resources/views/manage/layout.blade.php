ed9543584f3e7759742bb63f7b5776f3c0771c04
Ryu Seino <huntinghighandlow.rs@gmail.com>
2018-04-25 09:24:10 +0900

    enable Vue.js

--- resources/views/manage/layout.blade.php
+++ resources/views/manage/layout.blade.php
@@ -4,8 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
           integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <title>@yield('title')</title>
 
 </head>
